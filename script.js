aaf1da9896dd84bb31020d1e24bc62b139e693c0
estherpinos <estherpinos1@gmail.com>
2023-09-19 16:56:17 +0200

    Add [bombe red]

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const select=document.getElementById('inputGroupSelect01');
+const container= document.querySelector('.container');
 
 
 document.getElementById('play').addEventListener('click', function(){
@@ -8,7 +9,7 @@ document.getElementById('play').addEventListener('click', function(){
 
 
  //  Fare un switch per creare le diverse opzioni del gioco, che sarebbero facile:100 box, medio:81box e difficile 64box.
- const container= document.querySelector('.container');
+ 
  switch (scelta) {
 
    default:
@@ -18,9 +19,8 @@ document.getElementById('play').addEventListener('click', function(){
 
    case 1:
      reset();
-     creaDiv(100, "box1");
-    //  creaBombe();
-     console.log(creaBombe());
+     const listaBombe = creaBombe();
+     creaDiv(100, "box1", listaBombe);
      break;
      
 
@@ -40,49 +40,67 @@ document.getElementById('play').addEventListener('click', function(){
  
   }
 
-  function reset(){
-   container.innerHTML="";
-  }
+})
 
-  
 
-  function creaDiv(n, classe){
-   for (let cont = 1; cont <= n; cont++) {
-     const element = document.createElement('div');
-     element.innerHTML = `<span class="number">${cont}</span>`;
-     element.classList.add(classe);
-     element.addEventListener("click", function(){
-       element.classList.toggle("clicked");
-       console.log(this);
-     })
-     container.append(element);
-   }
-  }
 
-  
+// FUNZIONI
 
-  function creaBombe(){
-    const listaBombe=[];
-    for(let i=0; i<16; i++){
-      let numero = Math.ceil(Math.random() * 100 + 1);
-      let controlloNumeriDoppi = false;
-      for(let conta=0; conta<listaBombe.length; conta++){
-        if(listaBombe[conta]==numero)
-        {
-          controlloNumeriDoppi= true;
-          i--;
+function reset(){
+  container.innerHTML="";
+ }
+
+ 
+
+ function creaDiv(n, classe, listaBombe){
+  console.log(listaBombe)
+  for (let cont = 1; cont <= n; cont++) {
+    const element = document.createElement('div');
+    element.innerHTML = `<span class="number">${cont}</span>`;
+    element.classList.add(classe);
+    element.addEventListener("click", function(){
+      let indiceDiv = this.textContent;
+      let controlloBombe = false;
+      for(let i=0; i<listaBombe.length; i++){
+        if(listaBombe[i]==indiceDiv){
+          controlloBombe = true;
         }
       }
-      if(controlloNumeriDoppi==false)
-        listaBombe.push(numero);
-    } 
-
-    return listaBombe;
-    
+      if(controlloBombe==false){
+        element.classList.toggle("clicked");
+      }
+      else{
+        for(i=0; i<listaBombe.length; i++){
+          let element = listaBombe[i];
+          document.getElementsByClassName(classe)[element-1].classList.add("red");
+        }
+       
+      }
+   
+      
+    })
+    container.append(element);
   }
+ }
 
+ 
 
+ function creaBombe(){
+   const listaBombe=[];  // array dove verranno messi i numeri delle bombe
+   for(let i=0; i<16; i++){
+     let numero = Math.ceil(Math.random() * 100 + 1);
+     let controlloNumeriDoppi = false;
+     for(let conta=0; conta<listaBombe.length; conta++){
+       if(listaBombe[conta]==numero)
+       {
+         controlloNumeriDoppi= true;
+         i--;
+       }
+     }
+     if(controlloNumeriDoppi==false)
+       listaBombe.push(numero);
+   } 
 
-
-
-})
+   return listaBombe;
+   
+ }
